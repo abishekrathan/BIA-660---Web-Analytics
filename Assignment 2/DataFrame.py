bfd4ebf5766e3dd03af3ae7da858546137951f2e
abishekrathan <alakshm1@stevens.edu>
2017-02-09 17:30:48 -0500

    Update DataFrame.py
    Final code

--- Assignment 2/DataFrame.py
+++ Assignment 2/DataFrame.py
@@ -1,148 +1,220 @@
-import csv
+# Assignment-2
 
+# Abishek Lakshmirathan
+
+import datetime
+import csv
 from collections import OrderedDict
+from math import sqrt
 
 class DataFrame(object):
 
     @classmethod
+
     def from_csv(cls, file_path, delimiting_character=',', quote_character='"'):
-        """
-        Opens a file using the csv module.
-        https://docs.python.org/2/library/csv.html
-        :param file_path: a string representing the path to the file e.g. ~/Documents/textfile.txt
-        :param delimiting_character: a string representing the char(s) that separate columns in a row of data
-        :param quote_character: a string for the char(s) that surround values in a column, e.g. "value" -> "
-        :return: returns a DataFrame object with the data from the csv file at file_path
-        """
-        # opens a file in read, universal newline mode and store the file object in infile for this with block
         with open(file_path, 'rU') as infile:
-
-            # create a csv.reader object to process the file and store it in the variable reader
             reader = csv.reader(infile, delimiter=delimiting_character, quotechar=quote_character)
-
-            # creates a variable data and assigns it an empty list
-            data = []
-
-            # for each row read (past tense) in from the csv by reader
-            for row in reader:
-                # append a row (a list) to data
-                data.append(row)
-
-            # return an instantiated object of the current class (DataFrame if this is my original code)
-            # passing data into the list_of_lists argument
-            return cls(list_of_lists=data)
-        # end of with block, infile is closed automatically
+            data_collection = []
+            for index, row in enumerate(reader):
+                if index == 559:
+                    row[2] = row[2].replace(',', '')
+                data_collection.append(row)
+            return cls(list_of_lists=data_collection)
 
 
+    # Task 3
 
+    @classmethod
+    def get_column_type(self, column, col_collection):
+        is_int = True
+        for col in col_collection:
+            try:
+                if isinstance(int(col), int) is False:
+                    is_int = False
+            except ValueError:
+                is_int = False
+        if is_int:
+            return int
+        else:
+
+            # Checking if all items of the column are dates
+            is_date = True
+            # try:
+            for col in col_collection:
+                if isinstance(col, int):
+                   is_date = False
+                else:
+                    try:
+                        if not (isinstance(datetime.datetime.strptime(col, '%m/%d/%y %H:%M'), datetime.datetime)):
+                            is_date = False
+                    except ValueError:
+                        is_date = False
+            if is_date:
+
+            # converting each entry into Datetime objects
+
+                for index, entry in enumerate(col_collection):
+                    col_collection[index] = datetime.datetime.strptime(entry, '%m/%d/%y %H:%M')
+                return datetime
+            else:
+                raise TypeError('The values in the column are strings and this operation cannot be performed.')
+        return None
+
+
+    @classmethod
+    def min(self, column):
+        col_collection = [row[column] for row in df.data]
+        col_type=self.get_column_type(column, col_collection)
+        if col_type == int:
+            return min(col_collection)
+        elif col_type == datetime:
+            return min(col_collection)
+        else:
+            return None
+
+    @classmethod
+    def max(self, column):
+        col_collection = [row[column] for row in df.data]
+        col_type = self.get_column_type(column, col_collection)
+        if col_type == int:
+            return max(col_collection)
+        elif col_type == datetime:
+            return max(col_collection)
+        else:
+            return None
+
+    @classmethod
+    def median(self, column):
+        col_collection = [row[column] for row in df.data]
+        col_type = self.get_column_type(column, col_collection)
+        if col_type == int or col_type == datetime:
+            col_collection = sorted(col_collection)
+            if len(col_collection) % 2 == 1:
+                median = (len(col_collection)+1)/2
+                return col_collection[median]
+            else:
+                median1 = len(col_collection)/2
+                median2 = median1+1
+                return col_collection[(median1 + median2)/2]
+        else:
+            raise TypeError('The values in the column are strings and this operation cannot be performed.')
+
+    @classmethod
+    def mean(self, column):
+        col_collection = [row[column] for row in df.data]
+        col_type = self.get_column_type(column, col_collection)
+        sum=0
+        if col_type == int:
+            for col in col_collection:
+                sum = sum + int(col)
+            mean = sum/len(col_collection)
+            return mean
+        else:
+            raise TypeError('The values in the column are strings and this operation cannot be performed.')
+
+
+    @classmethod
+    def sum(self, column):
+        col_collection = [row[column] for row in df.data]
+        col_type = self.get_column_type(column, col_collection)
+        summation=0
+        if col_type == int:
+            for col in col_collection:
+                summation = summation + int(col)
+            return summation
+        else:
+            raise TypeError('The values in the column are strings and this operation cannot be performed.')
+
+    @classmethod
+    def std(self, column):
+        col_collection = [row[column] for row in df.data]
+        col_type = self.get_column_type(column, col_collection)
+        sum = 0
+        if (col_type == int):
+            for col in col_collection:
+                sum=sum+int(col)
+            num_items = len(col_collection)
+            mean = sum / num_items
+            differences = [int(x) - mean for x in col_collection]
+            sq_differences=0
+            for d in differences:
+                lis = [d ** 2 for d in differences]
+            for item in lis:
+                sq_differences = sq_differences + item
+            return sqrt(sq_differences/num_items)
+        else:
+            raise TypeError('The values in the column are strings and this operation cannot be performed.')
+
+    # Task 4
+    @classmethod
+    def add_rows(self, list_of_lists):
+        length=len(df.data[0])
+        for row in list_of_lists:
+            if(len(row) == length):
+                df.data.append(row)
+            else:
+                raise ValueError('The length of the row does not match to that of the existing data')
+        df.data=[OrderedDict(zip(df.header, row)) for row in df.data]
+
+    # Task 5
+    @classmethod
+    def add_columns(self,list_of_values,col_name):
+        length_rows_data = len(df.data)
+        length_values = len(list_of_values)
+        if(length_rows_data == length_values):
+            for index, headername in enumerate(df.header):
+                df.data[index][col_name]=list_of_values[index]
+            df.header.append(col_name)
+        else:
+            raise ValueError('The length of the column does not match to that of the existing data')
+
+    # Task 2
+    # Stripping whitespaces
     def __init__(self, list_of_lists, header=True):
-        """
-        The __init__ method is called anytime you instantiate the class.
-        E.g.
-        df = DataFrame(list_of_lists=some_list_of_lists)
-        :param self: this argument is implicitly passed, i.e. don't worry about it outside of this class definition.
-                    It is the object that you've instantiated.
-        :param list_of_lists: a list of lists, namely a list of rows, where each row is a list of values for columns
-                                in a dataset
-        :param header: a list of strings, where each string is a name of a column (in the same order as the columns)
-        """
-
-        # if what was passed into header is True or has a value that is equivalent to false, i.e. bool(header) is True
-        if header: # then do this
-
-            # set the header attribute of this DataFrame object that is being instantiated to the first row of what
-            # was passed into list_of_lists
+        for each_list in list_of_lists:
+            for index, word in enumerate(each_list):
+                each_list[index] = word.strip()
+        if header:
+            # Assigning header
             self.header = list_of_lists[0]
-            # set the data attribute of this DataFrame object to all the rows after the first row
-            # (remember things start from 0 in python not 1)
             self.data = list_of_lists[1:]
-
-        # if what was passed into header is False or has a value that is equivalent to false, i.e. bool(header) is False
-        else: # then do this
-            # set the data attr to list_of_lists (there's no header here)
+        else:
+            self.header = ['column' + str(index + 1) for index, column in enumerate(list_of_lists)]
             self.data = list_of_lists
 
-            # create a variable called generated_header and set it as an empty list
-            generated_header = []
+        # Task 1
+        # header name uniqueness
 
-            # choose the first row of the data set as a sample row to iterate through the columns, enumerate it so
-            # we can keep a count of what index we're at in the row
-            for index, column in enumerate(self.data[0]): # for each index and row in this first row of data
-                # append a string that is 'column' concatenated with a string of the current index + 1
-                generated_header.append('column' + str(index + 1))
-
-            # set our header attr to this generated_header
-            self.header = generated_header
-
-        # we're now outside of the if/else
-
-        # create an empty list called ordered_dict_rows
-        ordered_dict_rows = []
-
-        # for each row in self.data
-        for row in self.data:
-            # for each iteration of the above loop create an empty list called ordered_dict_data
-            ordered_dict_data = []
-
-            # for each index and value of this row in self.data
-            for index, row_value in enumerate(row):
-                # append a tuple to ordered_dict_data that contains the value in header that's at the same index as
-                # this value in row
-                ordered_dict_data.append((self.header[index], row_value))
-
-            # outside of the inner loop (for index, row_value in enumerate(row))
-            # ordered_dict_data now contains a list of tuples
-
-            # create an OrderedDict using ordered_dict_data and assign it to ordered_dict_row
-            # now we've converted this row to an OrderedDict!
-            ordered_dict_row = OrderedDict(ordered_dict_data)
-
-            # append ordered_dict_row to ordered_dict_rows
-            ordered_dict_rows.append(ordered_dict_row)
-
-        # now ordered_dict_rows has all the data from before but each row is an OrderedDict instead of just a list of
-        # values
-        # assign these to the data attr of this DataFrame object
-        self.data = ordered_dict_rows
-
-
+        is_unique = (all(list_of_lists[0].count(x) == 1 for x in list_of_lists[0]))
+        if not is_unique:
+            raise TypeError('Elements in the header are not unique!')
+        self.data = [OrderedDict(zip(self.header, row)) for row in self.data]
 
     def __getitem__(self, item):
-        """
-        the __getitem__ magic method is called whenenver you use square brackets on an object, e.g.  obj[item]
-        :param item: this is the object that is inside of the brackets, e.g. df[item]
-        :return: returns different things based on what item is, see below
-        """
-        # this is for rows only
-        # if item is an integer or a slice object
+        # for rows only
         if isinstance(item, (int, slice)):
             return self.data[item]
 
-        # this is for columns only
-        # if item is a string or unicode object
+        # for columns only
         elif isinstance(item, (str, unicode)):
-            return Series([row[item] for row in self.data])
+            return [row[item] for row in self.data]
 
-        # this is for rows and columns
-        # if item is a tuple
+        # for rows and columns
         elif isinstance(item, tuple):
             if isinstance(item[0], list) or isinstance(item[1], list):
-
                 if isinstance(item[0], list):
-                    rowz = [row for index, row in enumerate(self.data) if index in item[0]]
+                    row_collection = [row for index, row in enumerate(self.data) if index in item[0]]
                 else:
-                    rowz = self.data[item[0]]
-
+                    row_collection = self.data[item[0]]
                 if isinstance(item[1], list):
                     if all([isinstance(thing, int) for thing in item[1]]):
-                        return [[column_value for index, column_value in enumerate([value for value in row.itervalues()]) if index in item[1]] for row in rowz]
+                        return [[column_value for index, column_value in enumerate([value for value in row.itervalues()]) if index in item[1]] for row in row_collection]
                     elif all([isinstance(thing, (str, unicode)) for thing in item[1]]):
-                        return [[row[column_name] for column_name in item[1]] for row in rowz]
+                        return [[row[column_name] for column_name in item[1]] for row in row_collection]
                     else:
-                        raise TypeError('What the hell is this?')
-
+                        raise TypeError('Exception has occurred')
                 else:
-                    return [[value for value in row.itervalues()][item[1]] for row in rowz]
+                    return [[value for value in row.itervalues()][item[1]] for row in row_collection]
             else:
                 if isinstance(item[1], (int, slice)):
                     return [[value for value in row.itervalues()][item[1]] for row in self.data[item[0]]]
@@ -151,7 +223,7 @@ class DataFrame(object):
                 else:
                     raise TypeError('I don\'t know how to handle this...')
 
-        # only for lists of column names
+        # for lists of column names
         elif isinstance(item, list):
             return [[row[column_name] for column_name in item] for row in self.data]
 
@@ -159,62 +231,12 @@ class DataFrame(object):
         if index_only:
             return [index for index, row_value in enumerate(self[column_name]) if row_value==value]
         else:
-            return [row for row in self.data if row[column_name]==value]
-
-class Series(list):
-    def __init__(self, list_of_values):
-        self.data = list_of_values
-
-    def __eq__(self, other):
-        ret_list = []
-
-        for item in self.data:
-            ret_list.append(item == other)
-
-        return ret_list
-
-
+            return [row for row in self.data if row[column_name] == value]
 
 infile = open('SalesJan2009.csv')
 lines = infile.readlines()
 lines = lines[0].split('\r')
 data = [l.split(',') for l in lines]
 things = lines[559].split('"')
-data[559] = things[0].split(',')[:-1] + [things[1]] + things[-1].split(',')[1:]
-
-
+data[559] = things[0].split(',')[:-1] + [things[1].replace(',', '')] + things[-1].split(',')[1:]
 df = DataFrame(list_of_lists=data)
-# get the 5th row
-fifth = df[4]
-sliced = df[4:10]
-
-# get item definition for df [row, column]
-
-# get the third column
-tupled = df[:, 2]
-tupled_slices = df[0:5, :3]
-
-tupled_bits = df[[1, 4], [1, 4]]
-
-
-# adding header for data with no header
-df = DataFrame(list_of_lists=data[1:], header=False)
-
-# fetch columns by name
-named = df['column1']
-named_multi = df[['column1', 'column7']]
-
-#fetch rows and (columns by name)
-named_rows_and_columns = df[:5, 'column7']
-named_rows_and_multi_columns = df[:5, ['column4', 'column7']]
-
-
-#testing from_csv class method
-df = DataFrame.from_csv('SalesJan2009.csv')
-rows = df.get_rows_where_column_has_value('Payment_Type', 'Visa')
-indices = df.get_rows_where_column_has_value('Payment_Type', 'Visa', index_only=True)
-
-rows_way2 = df[indices, ['Product', 'Country']]
-
-list_of_bools = df['Payment_Type'] == 'Visa'
-df_boolean_indexed = df[list_of_bools]
